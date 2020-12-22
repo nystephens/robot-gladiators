90734c8770d22e25f042897eff0f15df5aae2870
Dead Nate <nathan.yukio.stephens@gmail.com>
2020-12-21 16:13:19 -0800

    console.log added for Robot Name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! Javascript is running!");
+//this code asks for robot's name.
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
