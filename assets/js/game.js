d9810a900287f9735908235af72717f5ea0f2795
Dead Nate <nathan.yukio.stephens@gmail.com>
2020-12-25 12:18:00 -0800

    added round alert and round end

--- assets/js/game.js
+++ assets/js/game.js
@@ -96,9 +96,22 @@ let fight = function(enemyName) {
 // }
 
  for(let i = 0; i <enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + ( i +  1));
+    
+    // pick new enemy to fight based on the index of the enemyNames array
     let pickedEnemyName = enemyNames[i];
+    // reset enemyHealth before starting new fight
     enemyHealth = 50; 
+    // use debugger to pause script from running and check whats going on
+    // debugger;
     fight(enemyNames[i]);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
  }
 
 
